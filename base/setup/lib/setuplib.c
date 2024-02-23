d42b047edb562c7ee6e21d92f84d3d3d462fa783
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2024-02-23 17:28:59 +0100

    ** WIP ** Improve handling of MUI/LANGID/... stuff
    This should fix part of CORE-11785
    
    ** WIP ** Continue massaging the MUI stuff AND actually start to remove GENERIC_LIST from the settings.
    
    Some stuff to see:
    https://github.com/reactos/reactos/commits/7af2f717bc6ecb99c151193117d990c291bf08aa/base/setup/usetup/settings.c
    https://github.com/reactos/reactos/commit/8f2c4f7a6dfed20dc887471b20a40528653edfa2
    etc.
    
    ** WIP ** Continue with refactoring the settings retrival and limiting usage of GENERIC_LIST.
    
    See commits:
    https://github.com/reactos/reactos/commit/829b0c63d9d756afcc395c00fea3d48145b8a8d1
    https://github.com/reactos/reactos/commit/67880e735241f1e071ac6aa537bd1e0c1b3b3825
    (for keyboard layout)
    
    and
    https://github.com/reactos/reactos/commit/e405ad257c0611d115f3fa7d82c9ca6cf00e6f75
    (a commit of mine)

--- base/setup/lib/setuplib.c
+++ base/setup/lib/setuplib.c
@@ -50,7 +50,7 @@ CheckUnattendedSetup(
     UnattendInf = SpInfOpenInfFile(UnattendInfPath,
                                    NULL,
                                    INF_STYLE_OLDNT,
-                                   pSetupData->LanguageId,
+                                   LANGIDFROMLCID(pSetupData->LocaleID),
                                    &ErrorLine);
     if (UnattendInf == INVALID_HANDLE_VALUE)
     {
@@ -179,10 +179,7 @@ CheckUnattendedSetup(
     {
         if (INF_GetData(&Context, NULL, &Value))
         {
-            LONG Id = wcstol(Value, NULL, 16);
-            RtlStringCchPrintfW(pSetupData->LocaleID,
-                                ARRAYSIZE(pSetupData->LocaleID),
-                                L"%08lx", Id);
+            pSetupData->LocaleID = (LCID)wcstoul(Value, NULL, 16);
             INF_FreeData(Value);
         }
     }
@@ -514,7 +511,7 @@ LoadSetupInf(
         SpInfOpenInfFile(FileNameBuffer,
                          NULL,
                          INF_STYLE_WIN4,
-                         pSetupData->LanguageId,
+                         LANGIDFROMLCID(pSetupData->LocaleID),
                          &ErrorLine);
     if (pSetupData->SetupInf == INVALID_HANDLE_VALUE)
         return ERROR_LOAD_TXTSETUPSIF;
@@ -1048,41 +1045,6 @@ VOID
 FinishSetup(
     IN OUT PUSETUP_DATA pSetupData)
 {
-    /* Destroy the computer settings list */
-    if (pSetupData->ComputerList != NULL)
-    {
-        DestroyGenericList(pSetupData->ComputerList, TRUE);
-        pSetupData->ComputerList = NULL;
-    }
-
-    /* Destroy the display settings list */
-    if (pSetupData->DisplayList != NULL)
-    {
-        DestroyGenericList(pSetupData->DisplayList, TRUE);
-        pSetupData->DisplayList = NULL;
-    }
-
-    /* Destroy the keyboard settings list */
-    if (pSetupData->KeyboardList != NULL)
-    {
-        DestroyGenericList(pSetupData->KeyboardList, TRUE);
-        pSetupData->KeyboardList = NULL;
-    }
-
-    /* Destroy the keyboard layout list */
-    if (pSetupData->LayoutList != NULL)
-    {
-        DestroyGenericList(pSetupData->LayoutList, TRUE);
-        pSetupData->LayoutList = NULL;
-    }
-
-    /* Destroy the languages list */
-    if (pSetupData->LanguageList != NULL)
-    {
-        DestroyGenericList(pSetupData->LanguageList, FALSE);
-        pSetupData->LanguageList = NULL;
-    }
-
     /* Close the Setup INF */
     SpInfCloseInfFile(pSetupData->SetupInf);
 }
@@ -1100,7 +1062,6 @@ UpdateRegistry(
     /**/IN BOOLEAN RepairUpdateFlag,     /* HACK HACK! */
     /**/IN PPARTLIST PartitionList,      /* HACK HACK! */
     /**/IN WCHAR DestinationDriveLetter, /* HACK HACK! */
-    /**/IN PCWSTR SelectedLanguageId,    /* HACK HACK! */
     IN PREGISTRY_STATUS_ROUTINE StatusRoutine OPTIONAL,
     IN PFONTSUBSTSETTINGS SubstSettings OPTIONAL)
 {
@@ -1113,6 +1074,11 @@ UpdateRegistry(
     BOOLEAN Success;
     BOOLEAN ShouldRepairRegistry = FALSE;
     BOOLEAN Delete;
+    LANGID SelectedLanguageId = LANGIDFROMLCID(pSetupData->LocaleID);
+
+__debugbreak();
+
+__debugbreak();
 
     if (RepairUpdateFlag)
     {
@@ -1226,7 +1192,7 @@ DoUpdate:
 
         if (!ImportRegistryFile(pSetupData->SourcePath.Buffer,
                                 File, Section,
-                                pSetupData->LanguageId, Delete))
+                                LANGIDFROMLCID(pSetupData->LocaleID), Delete))
         {
             DPRINT1("Importing %S failed\n", File);
             INF_FreeData(File);
@@ -1240,14 +1206,6 @@ DoUpdate:
     {
         /* See the explanation for this test above */
 
-        PGENERIC_LIST_ENTRY Entry;
-        PCWSTR LanguageId; // LocaleID;
-
-        Entry = GetCurrentListEntry(pSetupData->DisplayList);
-        ASSERT(Entry);
-        pSetupData->DisplayType = ((PGENENTRY)GetListEntryData(Entry))->Id;
-        ASSERT(pSetupData->DisplayType);
-
         /* Update display registry settings */
         if (StatusRoutine) StatusRoutine(DisplaySettingsUpdate);
         if (!ProcessDisplayRegistry(pSetupData->SetupInf, pSetupData->DisplayType))
@@ -1256,14 +1214,9 @@ DoUpdate:
             goto Cleanup;
         }
 
-        Entry = GetCurrentListEntry(pSetupData->LanguageList);
-        ASSERT(Entry);
-        LanguageId = ((PGENENTRY)GetListEntryData(Entry))->Id;
-        ASSERT(LanguageId);
-
         /* Set the locale */
         if (StatusRoutine) StatusRoutine(LocaleSettingsUpdate);
-        if (!ProcessLocaleRegistry(/*pSetupData->*/LanguageId))
+        if (!ProcessLocaleRegistry(pSetupData->LocaleID))
         {
             ErrorNumber = ERROR_UPDATE_LOCALESETTINGS;
             goto Cleanup;
@@ -1279,12 +1232,7 @@ DoUpdate:
 
         if (!IsUnattendedSetup)
         {
-            Entry = GetCurrentListEntry(pSetupData->LayoutList);
-            ASSERT(Entry);
-            pSetupData->LayoutId = ((PGENENTRY)GetListEntryData(Entry))->Id;
-            ASSERT(pSetupData->LayoutId);
-
-            /* Update keyboard layout settings with user-overridden values */
+            /* Update keyboard layout settings with user overriden values */
             // FIXME: Wouldn't it be better to do it all at once
             // with the AddKeyboardLayouts() step?
             if (StatusRoutine) StatusRoutine(KeybSettingsUpdate);
@@ -1322,7 +1270,7 @@ DoUpdate:
     if (SubstSettings)
     {
         /* HACK */
-        DoRegistryFontFixup(SubstSettings, wcstoul(SelectedLanguageId, NULL, 16));
+        DoRegistryFontFixup(SubstSettings, SelectedLanguageId);
     }
 #endif
 
